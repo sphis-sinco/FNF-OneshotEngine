19162b674f528648d1129b3c067107edd45a89a7
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-20 13:42:17 -0500

    libs.bat file for compiling

---
+++ libs.bat
@@ -0,0 +1,21 @@
+haxelib install lime 7.9.0
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install hscript
+haxelib install newgrounds
+haxelib install flixel-addons
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git extension-webm https://github.com/KadeDev/extension-webm
+haxelib run lime rebuild extension-webm windows
+haxelib install linc_luajit
+haxelib install actuate
+haxelib git extension-webm https://github.com/KadeDev/extension-webm
+lime rebuild extension-webm windows
+haxelib list
