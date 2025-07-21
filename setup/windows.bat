a07b1be9cd249bb02c29eac0cb392abe59eb8b1a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-21 14:53:52 -0500

    debug compiling fixesh

--- setup/windows.bat
+++ setup/windows.bat
@@ -19,5 +19,6 @@ haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c0
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
+haxelib install hxcpp-debug-server
 echo Finished!
 pause
