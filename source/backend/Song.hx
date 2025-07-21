165dff1403793f07c3f699317989dc9eb3d4f77b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-21 08:25:35 -0500

    for some reason mods work but dont work at the same time? I gotta fix that...

--- source/backend/Song.hx
+++ source/backend/Song.hx
@@ -2,7 +2,6 @@ package backend;
 
 import haxe.Json;
 import lime.utils.Assets;
-
 import objects.Note;
 
 typedef SwagSong =
@@ -25,7 +24,7 @@ typedef SwagSong =
 	@:optional var gameOverSound:String;
 	@:optional var gameOverLoop:String;
 	@:optional var gameOverEnd:String;
-	
+
 	@:optional var disableNoteRGB:Bool;
 
 	@:optional var arrowSkin:String;
@@ -66,13 +65,14 @@ class Song
 
 	public static function convert(songJson:Dynamic) // Convert old charts to psych_v1 format
 	{
-		if(songJson.gfVersion == null)
+		if (songJson.gfVersion == null)
 		{
 			songJson.gfVersion = songJson.player3;
-			if(Reflect.hasField(songJson, 'player3')) Reflect.deleteField(songJson, 'player3');
+			if (Reflect.hasField(songJson, 'player3'))
+				Reflect.deleteField(songJson, 'player3');
 		}
 
-		if(songJson.events == null)
+		if (songJson.events == null)
 		{
 			songJson.events = [];
 			for (secNum in 0...songJson.notes.length)
@@ -82,22 +82,24 @@ class Song
 				var i:Int = 0;
 				var notes:Array<Dynamic> = sec.sectionNotes;
 				var len:Int = notes.length;
-				while(i < len)
+				while (i < len)
 				{
 					var note:Array<Dynamic> = notes[i];
-					if(note[1] < 0)
+					if (note[1] < 0)
 					{
 						songJson.events.push([note[0], [[note[2], note[3], note[4]]]]);
 						notes.remove(note);
 						len = notes.length;
 					}
-					else i++;
+					else
+						i++;
 				}
 			}
 		}
 
 		var sectionsData:Array<SwagSection> = songJson.notes;
-		if(sectionsData == null) return;
+		if (sectionsData == null)
+			return;
 
 		for (section in sectionsData)
 		{
@@ -105,7 +107,8 @@ class Song
 			if (beats == null || Math.isNaN(beats))
 			{
 				section.sectionBeats = 4;
-				if(Reflect.hasField(section, 'lengthInSteps')) Reflect.deleteField(section, 'lengthInSteps');
+				if (Reflect.hasField(section, 'lengthInSteps'))
+					Reflect.deleteField(section, 'lengthInSteps');
 			}
 
 			for (note in section.sectionNotes)
@@ -113,17 +116,19 @@ class Song
 				var gottaHitNote:Bool = (note[1] < 4) ? section.mustHitSection : !section.mustHitSection;
 				note[1] = (note[1] % 4) + (gottaHitNote ? 0 : 4);
 
-				if(!Std.isOfType(note[3], String))
-					note[3] = Note.defaultNoteTypes[note[3]]; //compatibility with Week 7 and 0.1-0.3 psych charts
+				if (!Std.isOfType(note[3], String))
+					note[3] = Note.defaultNoteTypes[note[3]]; // compatibility with Week 7 and 0.1-0.3 psych charts
 			}
 		}
 	}
 
 	public static var chartPath:String;
 	public static var loadedSongName:String;
+
 	public static function loadFromJson(jsonInput:String, ?folder:String):SwagSong
 	{
-		if(folder == null) folder = jsonInput;
+		if (folder == null)
+			folder = jsonInput;
 		PlayState.SONG = getChart(jsonInput, folder);
 		loadedSongName = folder;
 		chartPath = _lastPath;
@@ -136,21 +141,25 @@ class Song
 	}
 
 	static var _lastPath:String;
+
 	public static function getChart(jsonInput:String, ?folder:String):SwagSong
 	{
-		if(folder == null) folder = jsonInput;
+		if (folder == null)
+			folder = jsonInput;
 		var rawData:String = null;
-		
+
 		var formattedFolder:String = Paths.formatToSongPath(folder);
 		var formattedSong:String = Paths.formatToSongPath(jsonInput);
 		_lastPath = Paths.json('$formattedFolder/$formattedSong');
 
 		#if MODS_ALLOWED
-		if(FileSystem.exists(_lastPath))
+		if (!FileSystem.exists(_lastPath))
+			_lastPath = Paths.modsJson('$formattedFolder/$formattedSong');
+		if (FileSystem.exists(_lastPath))
 			rawData = File.getContent(_lastPath);
 		else
 		#end
-			rawData = Assets.getText(_lastPath);
+		rawData = Assets.getText(_lastPath);
 
 		return rawData != null ? parseJSON(rawData, jsonInput) : null;
 	}
@@ -158,22 +167,23 @@ class Song
 	public static function parseJSON(rawData:String, ?nameForError:String = null, ?convertTo:String = 'psych_v1'):SwagSong
 	{
 		var songJson:SwagSong = cast Json.parse(rawData);
-		if(Reflect.hasField(songJson, 'song'))
+		if (Reflect.hasField(songJson, 'song'))
 		{
 			var subSong:SwagSong = Reflect.field(songJson, 'song');
-			if(subSong != null && Type.typeof(subSong) == TObject)
+			if (subSong != null && Type.typeof(subSong) == TObject)
 				songJson = subSong;
 		}
 
-		if(convertTo != null && convertTo.length > 0)
+		if (convertTo != null && convertTo.length > 0)
 		{
 			var fmt:String = songJson.format;
-			if(fmt == null) fmt = songJson.format = 'unknown';
+			if (fmt == null)
+				fmt = songJson.format = 'unknown';
 
-			switch(convertTo)
+			switch (convertTo)
 			{
 				case 'psych_v1':
-					if(!fmt.startsWith('psych_v1')) //Convert to Psych 1.0 format
+					if (!fmt.startsWith('psych_v1')) // Convert to Psych 1.0 format
 					{
 						trace('converting chart $nameForError with format $fmt to psych_v1 format...');
 						songJson.format = 'psych_v1_convert';
