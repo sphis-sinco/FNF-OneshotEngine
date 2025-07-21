11a7427b04313ac0f54fa4ae58fda17846b87462
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-20 21:18:46 -0500

    SONG_SELECTION define

--- source/ose/FinishedState.hx
+++ source/ose/FinishedState.hx
@@ -12,20 +12,30 @@ class FinishedState extends FlxState
 		var text:FlxText = new FlxText(0, 0, 0, 'Yo! You finished the oneshot mod, congrats bro.', 16);
 		if (InitState.songs.length > 1)
 		{
-			text.text += '\nThere are ${InitState.songs.length - 1} more songs though, so you and try to get those if you want.'
-				+ '\nIt\'s random chance unless coded otherwise.'
-				+ '\n\n(Press ENTER to go back to InitState and try to get those other songs)';
+			text.text += '\nThere are ${InitState.songs.length - 1} more songs though, '
+			#if SONG_SELECTION
+			+ 'so you can play those if you want.'
+			#else
+			+ 'so you and try to get those if you want.' + '\nIt\'s random chance unless coded otherwise.'
+			#end
+			+ '\n\n(Press ENTER to go back to InitState and '
+			#if SONG_SELECTION
+			+ ' play those other songs)';
+			#else
+			+ ' try to get those other songs)';
+			#end
 		}
 		text.screenCenter();
 		add(text);
 	}
 
-        override function update(elapsed:Float) {
-                super.update(elapsed);
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
 
-                if (InitState.songs.length > 1 && FlxG.keys.justReleased.ENTER)
-                {
-                        FlxG.switchState(new InitState());
-                }
-        }
+		if (InitState.songs.length > 1 && FlxG.keys.justReleased.ENTER)
+		{
+			FlxG.switchState(new InitState());
+		}
+	}
 }
